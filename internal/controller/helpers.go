b2feb3d584c704a50e3919f6c2a79991c3425ea7
jpvargasdev <vargasm.jp@gmail.com>
2025-07-08 16:09:31 +0200

    fix: remove auth for now

--- internal/controller/helpers.go
+++ internal/controller/helpers.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"context"
 	"guilliman/internal/models"
-	"guilliman/internal/utils"
 	"net/http"
 	"time"
 
@@ -11,11 +10,6 @@ import (
 )
 
 func (h *Controller) RecalculateAllAccountBalances(c *gin.Context) {
-	_, err := utils.GetUserUID(c)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
 	// Start a new context with a timeout for the operation
 	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
 	defer cancel()
